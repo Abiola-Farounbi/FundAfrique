3dccb44a911d05595f10827aa82fba42ed97d4e9
abiola farounbi <farounbiabiola@gmail.com>
2021-09-27 20:03:20 +0100

    Fixed the responsiveness

--- frontend/src/components/Footer.vue
+++ frontend/src/components/Footer.vue
@@ -1,21 +1,65 @@
 <template>
-   <Footer class='flex justify-evenly item-center'>
-     <div>
+<Footer >
+  <div class='mt-10 py-4 px-16 sm:px-40 block sm:flex justify-evenly item-center'>
+     <div classs='my-4'>
            <router-link class='logo'  to="/"> FundAfrique </router-link>
      </div>
 
-     <div>
+     <div classs='my-4'>
          <h3 class='text-primary text-lg font-bold'> African Divisions </h3>
-         <li class='text-secondary text-base'> North Africa </li>
-         <li> North Africa </li>
-         <li> North Africa </li>
-         <li> North Africa </li>
-         <li> North Africa </li>
+         <li class='list-none text-secondary text-base'> North Africa </li>
+         <li  class='list-none text-secondary text-base'> East Africa </li>
+         <li  class='list-none text-secondary text-base'> Central Africa </li>
+         <li  class='list-none text-secondary text-base' >West Africa </li>
+         <li  class='list-none text-secondary text-base'>South Africa </li>
      </div>
 
-     <div></div>
+     <div classs='my-4'>
+         <h3 class='text-primary text-lg font-bold'> Learn More </h3>
+         <li class='list-none text-secondary text-base'> How it works </li>
+         <li class='list-none text-secondary text-base'> Success stories </li>
+         <li class='list-none text-secondary text-base'> FAQs </li>
+     </div>
 
-     <div></div>
+     <div classs='my-4'>
+         <h3 class='text-primary text-lg font-bold'> Resources </h3>
+         <li class='list-none text-secondary text-base'> Contact center </li>
+          <li class='list-none text-secondary text-base'> About </li>
+     </div>
+
+     </div>
+      <div class=' border-t py-8 mx-16 sm:mx-40  block sm:flex justify-evenly item-center'>
+         <p class="my-4 "> © 2010-2021 FundAfrique </p>
+         <p  class="my-4 "> Privacy </p>
+         <p  class="my-4 "> Terms</p>
+          <p  class="my-4 "> Legal </p>
+          <ul class="my-4 flex">
+              <li class='list-none mx-2'>
+                <svg xmlns="http://www.w3.org/2000/svg" width="18.472" height="18.472" viewBox="0 0 18.472 18.472">
+              <path id="Icon_metro-instagram" data-name="Icon metro-instagram" d="M17.976,1.928H5.637A3.076,3.076,0,0,0,2.571,4.995V17.333A3.076,3.076,0,0,0,5.637,20.4H17.976a3.076,3.076,0,0,0,3.067-3.067V4.995A3.076,3.076,0,0,0,17.976,1.928ZM8.484,10.009h6.645a3.518,3.518,0,1,1-6.645,0Zm10.25,0v6.926a1.158,1.158,0,0,1-1.154,1.154H6.034A1.158,1.158,0,0,1,4.88,16.936V10.009H6.685a5.249,5.249,0,1,0,10.242,0Zm0-4.042a.579.579,0,0,1-.577.577H17a.579.579,0,0,1-.577-.577V4.814A.579.579,0,0,1,17,4.237h1.154a.579.579,0,0,1,.577.577Z" transform="translate(-2.571 -1.928)" fill="#269e26"/>
+            </svg>
+              </li>
+
+              <li class="list-none  mx-2">
+                <svg xmlns="http://www.w3.org/2000/svg" width="21.556" height="17.507" viewBox="0 0 21.556 17.507">
+                <path id="Icon_awesome-twitter" data-name="Icon awesome-twitter" d="M19.34,7.744c.014.191.014.383.014.574a12.484,12.484,0,0,1-12.57,12.57A12.485,12.485,0,0,1,0,18.905a9.139,9.139,0,0,0,1.067.055,8.848,8.848,0,0,0,5.485-1.888,4.426,4.426,0,0,1-4.131-3.064,5.571,5.571,0,0,0,.834.068,4.673,4.673,0,0,0,1.163-.15A4.419,4.419,0,0,1,.875,9.59V9.536a4.449,4.449,0,0,0,2,.561A4.425,4.425,0,0,1,1.5,4.188a12.558,12.558,0,0,0,9.109,4.623A4.987,4.987,0,0,1,10.5,7.8,4.422,4.422,0,0,1,18.15,4.776a8.7,8.7,0,0,0,2.8-1.067,4.406,4.406,0,0,1-1.942,2.435,8.857,8.857,0,0,0,2.544-.684A9.5,9.5,0,0,1,19.34,7.744Z" transform="translate(0 -3.381)" fill="#269e26"/>
+              </svg>
+              </li>
+
+              <li class="list-none  mx-2">
+                <svg xmlns="http://www.w3.org/2000/svg" width="20.883" height="20.756" viewBox="0 0 20.883 20.756">
+                <path id="Icon_awesome-facebook" data-name="Icon awesome-facebook" d="M21.445,11A10.441,10.441,0,1,0,9.372,21.319v-7.3H6.72V11H9.372V8.7c0-2.617,1.558-4.062,3.944-4.062a16.069,16.069,0,0,1,2.338.2V7.413H14.337a1.509,1.509,0,0,0-1.7,1.631V11h2.9l-.463,3.018H12.635v7.3A10.445,10.445,0,0,0,21.445,11Z" transform="translate(-0.563 -0.563)" fill="#269e26"/>
+              </svg>
+              </li>
+
+              <li class="list-none  mx-2">
+              <svg xmlns="http://www.w3.org/2000/svg" width="18.862" height="18.861" viewBox="0 0 18.862 18.861">
+                <path id="Icon_awesome-linkedin-in" data-name="Icon awesome-linkedin-in" d="M4.222,18.862H.312V6.269h3.91ZM2.265,4.552A2.275,2.275,0,1,1,4.529,2.265,2.284,2.284,0,0,1,2.265,4.552ZM18.858,18.862h-3.9v-6.13c0-1.461-.029-3.334-2.033-3.334-2.033,0-2.345,1.587-2.345,3.229v6.235H6.672V6.269h3.75V7.987h.055a4.109,4.109,0,0,1,3.7-2.034c3.958,0,4.685,2.606,4.685,5.991v6.917Z" transform="translate(0 0)" fill="#269e26"/>
+              </svg>
+              </li>
+          </ul>
+      </div>
+  
    </Footer>
 </template>
 
